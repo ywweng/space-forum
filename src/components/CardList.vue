78bc902d413ffafce46925b3d077b4ddab10403d
yuwen <yuwennweng@gmail.com>
2022-05-21 23:58:47 +0800

    update: CardList

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,27 +1,108 @@
-<script setup></script>
+<script>
+  import api from '@/utils/api'
+  import { fromNow, setAvatar } from '@/utils/mixin'
+  import { ref, toRef, watch, onMounted } from 'vue'
+  import { useRouter } from 'vue-router'
+
+  export default {
+    props: {
+      searchData: {
+        type: [Object, Array],
+        default: {}
+      }
+    },
+    setup(props) {
+      const isLoading = ref(true)
+      const posts = ref([])
+      const totalPage = ref(1)
+      const currentPage = ref(1)
+
+      const fetchPosts = async (page = 1) => {
+        try {
+          const { pages, data } = await api.getPosts(page)
+          if (page === 1) {
+            posts.value = data
+            totalPage.value = pages
+          } else {
+            posts.value.push(...data)
+          }
+          isLoading.value = false
+        } catch (error) {}
+      }
+      onMounted(() => {
+        fetchPosts()
+      })
+
+      const more = ref('More...')
+      const morePage = () => {
+        if (currentPage.value >= totalPage.value) {
+          more.value = 'No more!'
+          return
+        } else {
+          currentPage.value++
+        }
+      }
+      watch(currentPage, () => {
+        if (currentPage.value > totalPage.value) return
+        fetchPosts(currentPage.value)
+      })
+
+      const searchData = toRef(props, 'searchData')
+      watch(searchData, () => {
+        currentPage.value = 1
+        totalPage.value = Math.ceil(searchData.value.length / 10)
+        posts.value = searchData.value
+      })
+
+      return {
+        setAvatar,
+        fromNow,
+        isLoading,
+        posts,
+        currentPage,
+        morePage,
+        more
+      }
+    }
+  }
+</script>
 
 <template>
-  <div class="space d-flex flex-column my-3 p-1" @click="$router.push('/space/1')">
+  <div
+    class="d-flex justify-content-center align-items-center vh-100"
+    v-if="isLoading"
+  >
+    <div class="spinner-border" role="status" style="width: 3rem; height: 3rem">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+  <div
+    class="space d-flex flex-column my-3 p-1"
+    @click="$router.push(`/space/${post.id}`)"
+    v-for="post in posts"
+    :key="post.id"
+    v-else
+  >
     <div
       class="space-header d-flex justify-content-between align-items-center p-3"
     >
       <div class="info d-flex my-auto">
         <img
-          src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/4f/4f6464cfec3bc282717611e9f936c09c5b17572d_full.jpg"
+          :src="setAvatar(post.user.gender)"
           class="avatar me-3 rounded-circle"
         />
 
-        <span class="nickname fs-4">Nick</span>
-        <span class="id">#2</span>
+        <span class="nickname fs-4">{{ post.user.name }}</span>
+        <span class="id">#{{ post.user.id }}</span>
       </div>
       <div class="datetime">
-        <span class="justify-content-center">2022-1-23 10:15</span>
+        <span class="justify-content-center">{{ fromNow(post.date) }}</span>
       </div>
     </div>
     <div class="space-body px-3">
-      <h5 class="title fs-5">不想好了過幾現在嗚先</h5>
+      <h5 class="title fs-5">{{ post.title }}</h5>
       <span class="preview">
-        不想好了過幾現在嗚先，實像是的活喔喔喔看過年真的，油晚上會讓我的那我，碰的畫面很帥我們的資訊，什麼知道我個。已經是的好可能要是直，為了需該不本來起是那個，是把是太好都不想。
+        {{ post.content }}
       </span>
     </div>
     <div class="space-footer d-flex justify-content-around py-1">
@@ -29,16 +110,24 @@
         <button type="button" class="pt-2 action">
           <ion-icon name="heart-outline"></ion-icon>
         </button>
-        <span class="m-2 like-count">1100</span>
+        <span class="m-2 like-count">{{ post.likes }}</span>
       </div>
       <div class="d-flex align-center">
         <button type="button" class="pt-2 action">
           <ion-icon name="chatbubbles-outline"></ion-icon>
         </button>
-        <span class="m-2 comment-count">10</span>
+        <span class="m-2 reply-count">{{ post.replies }}</span>
       </div>
     </div>
   </div>
+  <button
+    type="button"
+    class="btn more p-2 text-center w-100"
+    @click="morePage"
+    :disabled="more === 'No more!'"
+  >
+    {{ more }}
+  </button>
 </template>
 
 <style lang="scss" scoped>
@@ -58,5 +147,9 @@
       border: none;
       background: inherit;
     }
+  }
+
+  .more {
+    @extend %glassBg;
   }
 </style>
