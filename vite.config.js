cf4e36ee39876e789c3c9607b6391f515c2f945e
yuwen <yuwennweng@gmail.com>
2022-05-25 23:33:07 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'/space-forum-vue/',
+  base:'/space-forum/',
   resolve: {
     alias: { '@/': `${path.resolve(__dirname, 'src')}/` },
   },
